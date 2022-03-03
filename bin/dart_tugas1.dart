6b12407b9ab91790177fb13d8bcd1306ce0d4001
Delicate23 <73923266+Delicate23@users.noreply.github.com>
2022-03-03 11:37:45 +0700

    input user

--- bin/dart_tugas1.dart
+++ bin/dart_tugas1.dart
@@ -1,5 +1,9 @@
 import 'dart:io';
 
 void main(List<String> arguments) {
-  
+  String? input = stdin.readLineSync();
+  int number = 0;
+  if(input != null){
+    number = int.parse(input);
+  }
 }
