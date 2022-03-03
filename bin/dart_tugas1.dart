42ee33746aa72791190b73062f12f42572e2fa42
Delicate23 <73923266+Delicate23@users.noreply.github.com>
2022-03-03 12:49:15 +0700

    result input genap

--- bin/dart_tugas1.dart
+++ bin/dart_tugas1.dart
@@ -6,4 +6,29 @@ void main(List<String> arguments) {
   if(input != null){
     number = int.parse(input);
   }
+  if(number%2==0) {
+    int left = 1;
+    int right = 4;
+    for(int i = 0; i < number; i++)
+      {
+          for(int j = 0; j < (2 * number); j++)
+          {
+              if(left > 4){
+                left = 1;
+              }
+              if(right < 1){
+                right = 4;
+              }
+              if(i + j <= number - 1)  // upper left triangle
+                  stdout.write(left++);
+              else
+                  stdout.write(" ");
+              if((i + number) <= j)  // upper right triangle
+                  stdout.write(right--);
+              else
+                  stdout.write(" ");
+          }
+          print("");
+      }
+  }
 }
